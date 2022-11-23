b98dce20b854ebc904b67667b9091ada10adcfe2
GAlack15 <109077380+GAlack15@users.noreply.github.com>
2022-11-23 16:33:13 -0500

    Wizert moves error
    im so frustrated, i can make wizert change his location in the array but this requires a long if else statement. i wish there was an easier way to do this. I cant seem to just make it so wizert can have the option to move without a long hard coding

--- CIS129FinalProject/Class1.cs
+++ CIS129FinalProject/Class1.cs
@@ -8,13 +8,13 @@ namespace CIS129FinalProject
 {
     internal class Class1
     {
-        static void Main(string[] args) 
+        static void Main(string[] args)
         {
             Wizert wizert = new Wizert(3, 0, 10, 10);
             int[,] array = new int[5, 5];
             int yourLocation = array[wizert.xPos, wizert.yPos];
             // enemies go here
-            Enemies Goblin = new Enemies(1, 3, 3);
+            Enemies Goblin = new Enemies(1, 0, 3);
             int goblinLocation = array[Goblin.xPos, Goblin.yPos];
             Enemies Orc = new Enemies(3, 2, 5);
             int orcLocation = array[Orc.xPos, Orc.yPos];
@@ -27,7 +27,41 @@ namespace CIS129FinalProject
             Console.WriteLine("You the all powerful Wizert sought out new magical knowledge");
             Console.WriteLine("after successfully obtaining what you sought and completing the quest");
             Console.WriteLine("you find the only way to leave the Sorcerer's Tower, is to go through an old dungeon");
-            Console.WriteLine(" you have entered the dungeon and you cannot go back, do you want to go 1 for north, 2 for west, and 3 for east?");
+            Console.WriteLine(" you have entered the dungeon and you cannot go back a wall closes behined you, do you want to go 1 for north, 2 for west, and 3 for east?");
+            Console.WriteLine(wizert.xPos);
+            Console.WriteLine(wizert.yPos);
+            string UserNum = Console.ReadLine();
+
+            // first if statement starts
+            if (UserNum == "1")
+                {
+                    Console.WriteLine(" Wizert Moves up one space in the array");
+                    wizert.yPos = wizert.yPos + 1;
+                    Console.WriteLine(wizert.yPos);
+                    Console.WriteLine(wizert.xPos);
+                }
+                else if (UserNum == "2")
+                {
+                    Console.WriteLine(" Wizert Moves west one space in the array");
+                    wizert.xPos = wizert.xPos - 1;
+                    Console.WriteLine(wizert.xPos);
+                    Console.WriteLine(wizert.yPos);
+                   }
+                else if (UserNum == "3")
+                {
+                    Console.WriteLine("Wizert moves one space to the east in the array");
+                    wizert.xPos = wizert.xPos + 1;
+                    Console.WriteLine(wizert.xPos);
+                    Console.WriteLine(wizert.yPos);
+                }
+    //first if statement comes to an end
+
+
+                // first attack statement
+                if (wizert.xPos == 2 && wizert.yPos == 0) {
+                Console.WriteLine(" while Adventuring Wizert encounters a goblin, press 1 for attack, 2 for heal, 3 to flee");
+
+                }
         }
     }
 }
---
+++ CIS129FinalProject/Class2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS129FinalProject
+{
+    internal class Class2
+    {
+        //public void MoveWizert()
+        //{
+        //    Console.WriteLine("your ready to continue your travels through the dungeon, do you want to press 1 to go north, press 2 to go west, press 3 to go east or press 4 to go south.");
+
+        //    if (UserNum == "1")
+        //    {
+        //        Console.WriteLine(" Wizert Moves up one space in the array");
+        //        wizert.yPos = wizert.yPos + 1;
+        //        Console.WriteLine(wizert.yPos);
+        //        Console.WriteLine(wizert.xPos);
+        //    }
+        //    else if (UserNum == "2")
+        //    {
+        //        Console.WriteLine(" Wizert Moves west one space in the array");
+        //        wizert.xPos = wizert.xPos - 1;
+        //        Console.WriteLine(wizert.xPos);
+        //        Console.WriteLine(wizert.yPos);
+        //    }
+        //    else if (UserNum == "3")
+        //    {
+        //        Console.WriteLine("Wizert moves one space to the east in the array");
+        //        wizert.xPos = wizert.xPos + 1;
+        //        Console.WriteLine(wizert.xPos);
+        //        Console.WriteLine(wizert.yPos);
+
+        //    }
+        //    else if (UserNum == "4")
+        //    {
+        //        Console.WriteLine("Wizert moves one space to the south in the array");
+        //        wizert.yPos = wizert.yPos - 1;
+        //        Console.WriteLine(wizert.yPos);
+        //        Console.WriteLine(wizert.xPos);
+        //    }
+        }
+    }
+
